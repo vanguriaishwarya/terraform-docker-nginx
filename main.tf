00f9af5519c339adffd5ef1356030615d5f131a1
V. Aishwarya <aishwaryavanguri07@gmail.com>
2025-06-07 14:15:47 +0530

    Initial commit - Terraform Docker NGINX setup

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_terraform"
+  image = docker_image.nginx.name
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
